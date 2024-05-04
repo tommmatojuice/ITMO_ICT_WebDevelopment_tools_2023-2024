bca59fde22d2044320dd2bd3a10ebe5ff6bf596a
tommmatojuice <nastya.levashova.12@mail.ru>
2024-05-04 20:25:43 +0300

    Added second laboratory work

---
+++ students/K33391/Levashova_Anastasiia/LR2/task_2/config.py
@@ -0,0 +1,14 @@
+URLS = [
+    'https://skillbox.ru/media/code/kak-stat-tsennym-kadrom-6-sovetov-dlya-karernogo-rosta-razrabotchika/',
+    'https://skillbox.ru/media/design/kak-stat-dizaynerom-luchshie-professionalnye-kursy-graficheskogo-dizayna/',
+    'https://skillbox.ru/media/management/kak-stat-prodaktmenedzherom-i-nuzhno-li-dlya-etogo-obrazovanie/',
+    'https://skillbox.ru/media/management/produktovyy-analitik-chem-on-zanimaetsya-skolko-zarabatyvaet-i-kak-im-stat/',
+    'https://skillbox.ru/media/code/qainzhener-kto-eto-chem-on-zanimaetsya-i-kak-im-stat/',
+    'https://skillbox.ru/media/management/kto-takoy-restorator-skolko-on-zarabatyvaet-i-kak-im-stat/',
+    'https://skillbox.ru/media/marketing/kak-stat-eventmenedzherom-gde-uchitsya-i-poluchat-prakticheskie-navyki/',
+    'https://skillbox.ru/media/marketing/kak-stat-seospetsialistom-i-chto-osvoit-chtoby-zarabatyvat-bolshe-80-tysyach-rubley/'
+    ]
+
+NUM_THREADS = 4
+HTML_CLASS = 'stk-reset stk-theme_26309__style_large_header'
+HTML_TAG = 'h2'
